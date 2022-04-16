71fc7ebe8a74331ee6147ef82ccce29228d29328
Lexho1 <95334377+Lexho1@users.noreply.github.com>
2022-04-17 01:36:30 +0800

    Update _.js

--- _.js
+++ _.js
@@ -1,6 +1,6 @@
 const jsname = '腾讯自选股'
 const $ = Env(jsname)
-const notifyFlag = 1; //0为关闭通知，1为打开通知,默认为1
+const notifyFlag = 0; //0为关闭通知，1为打开通知,默认为1
 
 const notify = $.isNode() ? require('./sendNotify') : '';
 
